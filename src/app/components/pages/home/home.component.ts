3943a6461b5f492b91fe6f1c495b9889e7bdd43b
João Henrique <joaohenriquevidalribeiro@gmail.com>
2024-08-27 17:36:51 -0300

    adding the ngOninit to catch the userData

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  nome!: string;
+  departamento!: string;
+  contato!: string;
+
+
+  user = {} as User;
+  users: User[] = [];
+  constructor(private formBuilder: FormBuilder,
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+    ){
+
+}
+
+  ngOnInit() {
+    const username = localStorage.getItem('username');
+
+    if (username) {
+      this.userService.getUserByUsername(username).subscribe({
+        next: (userData) => {
+          if (userData) {
+            this.nome = userData.name;
+            this.departamento = userData.departamento.name;
+            this.contato = userData.contato;
+
+          } else {
+            console.log('Usuário não encontrado.');
+          }
+        },
+        error: (error) => {
+          console.error('Erro ao obter informações do usuário:', error);
+        }
+      });
+    } else {
+      console.log('Nome de usuário não encontrado no localStorage.');
+    }
+  }
 
 }
